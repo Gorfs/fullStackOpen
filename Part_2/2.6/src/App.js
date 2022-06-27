094c765aa4cef62227522a29ccb9487497d01d9f
Gorfgorf23 <archiebeales@gmail.com>
2022-06-27 22:27:39 +0200

    finished 2.8

--- Part_2/2.6/src/App.js
+++ Part_2/2.6/src/App.js
@@ -1,35 +1,41 @@
 import { useState } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
-  const [newName, setNewName] = useState("")
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+  ])
+  const [newInfo, setNewInfo] = useState({ name: "", number: "" })
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log("the submit button was pressed", event.target)
+    console.log("the submit button was pressed!")
     //checking to see if the name already exists
-    console.log("checking if ", event.target.value, " is in the array")
-    if (
-      persons.map((person) => person.name).indexOf(event.target.value) === -1
-    ) {
+    console.log("checking if ", newInfo.name, " is in the array")
+    if (persons.map((person) => person.name).indexOf(newInfo.name) !== -1) {
       console.log(
         "Name already in the Array, please dont use the same name again!"
       )
       alert("Name is already in the array!")
     } else {
       const personObject = {
-        name: newName,
+        name: newInfo.name,
+        number: newInfo.number,
       }
       setPersons(persons.concat(personObject))
-      setNewName("")
+      setNewInfo({ name: "", number: "" })
       console.log("pushed a new person into the list")
       console.log("set the newName to nothing")
     }
   }
 
   const handleChange = (event) => {
-    console.log("setting a new name", event.target.value)
-    setNewName(event.target.value)
+    if (event.target.name === "name") {
+      console.log("changing the name")
+      setNewInfo({ name: event.target.value, number: newInfo.number })
+    } else {
+      console.log("changing the number")
+      setNewInfo({ name: newInfo.name, number: event.target.value })
+    }
   }
 
   return (
@@ -37,18 +43,23 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name:{" "}
+          <input value={newInfo.name} name="name" onChange={handleChange} />
         </div>
         <div>
-          <button type="submit" value={newName}>
-            add
-          </button>
+          number:{" "}
+          <input name="number" value={newInfo.number} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
     </div>
