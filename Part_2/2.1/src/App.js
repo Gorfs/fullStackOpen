5c67c4e026fbd78db07ba4ee8cd027c702097481
Gorfgorf23 <archiebeales@gmail.com>
2022-06-27 16:31:32 +0200

    finished 2.4

--- Part_2/2.1/src/App.js
+++ Part_2/2.1/src/App.js
@@ -39,49 +39,67 @@ const Course = ({ course }) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total
+        total={course.parts
+          .map((part) => part.exercises)
+          .reduce((p, s) => p + s)}
+      />
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Archie s course on how to be a good at parenting",
-        exercises: 3,
-        id: 4,
-      },
-    ],
-  }
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
   return (
     <div>
-      <Course course={course} />
-      <Total
-        total={course.parts
-          .map((part) => part.exercises) //creates an array with all the values of exercises in it
-          .reduce(
-            //uses reduce to calculate the sum of all the exercises
-            (previousValue, currentValue) => previousValue + currentValue
-          )}
-      />
+      {courses.map((course, i) => {
+        return <Course course={course} key={i} />
+      })}
+      {/*  <Total total={total} />*/}
     </div>
   )
 }
