3f287fd2fa466f6427dfac099d144d7405747609
Gorfgorf23 <archiebeales@gmail.com>
2022-07-03 17:25:22 +0200

    re updated part4/notes

---
+++ Part_4/notes/controllers/notes.js
@@ -0,0 +1,57 @@
+const notesRouter = require("express").Router()
+const Note = require("../models/note")
+
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+})
+
+notesRouter.get("/:id", async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+notesRouter.post("/", async (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  try {
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+  } catch (exception) {
+    next(exception)
+  }
+})
+notesRouter.delete("/:id", async (request, response, next) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+notesRouter.put("/:id", (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = notesRouter
